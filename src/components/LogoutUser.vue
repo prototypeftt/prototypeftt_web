0ebab699cc4472601db22c491ae593811e039434
dcreedon <damian.creedon@gmail.com>
2022-10-11 18:08:54 +0100

    Improve Logout feature

--- src/components/LogoutUser.vue
+++ src/components/LogoutUser.vue
@@ -1,6 +1,31 @@
 <template>
-    <div class="logout">
-      <h1>User Logged Out</h1>
-    </div>
-  </template>
-  
+  <div class="logout">
+    <h1>User Logged Out</h1>
+  </div>
+</template>
+
+<script>
+
+import { getAuth, signOut } from "firebase/auth";
+
+const auth = getAuth();
+console.log("logging out" + auth.currentUser.email);
+//auth.signOut();
+
+signOut(auth).then(() => {
+      console.log("logged out");
+    }).catch((error) => {
+      // An error happened.
+      console.log("error:" + error);
+    })
+
+export default {
+  name: 'LogoutUser',
+  props: {
+  }, methods: {
+    
+
+  }
+};
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -53,7 +53,7 @@ router.beforeEach((to, from, next) => {
     if (user) {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/firebase.User
-      alert('user state changed' + user)
+      //alert('user state changed' + user)
 
       next()
     } else {
